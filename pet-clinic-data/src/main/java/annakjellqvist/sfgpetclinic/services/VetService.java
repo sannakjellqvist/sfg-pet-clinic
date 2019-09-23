4539c2e1552f6cc6b0c2d7c7c4b1c7e48d6eca0e
Sanna Kjellqvist (ccz158) <sanna.kjellqvist@teliacompany.com>
2019-09-23 15:16:36 +0200

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/annakjellqvist/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/annakjellqvist/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package annakjellqvist.sfgpetclinic.services;
 
 import sannakjellqvist.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
