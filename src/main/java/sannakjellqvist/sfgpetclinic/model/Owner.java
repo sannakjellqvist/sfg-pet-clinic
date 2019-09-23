f33ddf3547158379e23b9eff5b34ea1725ab928c
Sanna Kjellqvist (ccz158) <sanna.kjellqvist@teliacompany.com>
2019-09-23 10:03:48 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/sannakjellqvist/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package sannakjellqvist.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/sannakjellqvist/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package sannakjellqvist.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
