c05572a69663d530ad5ecb3f2c68cc0af1226948
Sanna Kjellqvist (ccz158) <sanna.kjellqvist@teliacompany.com>
2019-09-25 09:52:00 +0200

    Added specialities map impl

---
+++ pet-clinic-data/src/main/java/sannakjellqvist/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package sannakjellqvist.sfgpetclinic.services;
+
+import sannakjellqvist.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
