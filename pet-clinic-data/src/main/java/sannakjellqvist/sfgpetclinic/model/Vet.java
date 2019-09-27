683071c6fc1e4bda17b875a9cba04446fa892b8b
Sanna Kjellqvist (ccz158) <sanna.kjellqvist@teliacompany.com>
2019-09-27 13:35:42 +0200

    Added list view for vets

--- pet-clinic-data/src/main/java/sannakjellqvist/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/sannakjellqvist/sfgpetclinic/model/Vet.java
@@ -20,8 +20,5 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
 
 }
