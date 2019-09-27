8311feedb967a9d9c95162cf4012db27442be9a1
Sanna Kjellqvist (ccz158) <sanna.kjellqvist@teliacompany.com>
2019-09-27 13:49:19 +0200

    Added json endpoint

--- pet-clinic-web/src/main/java/sannakjellqvist/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sannakjellqvist/sfgpetclinic/controllers/VetController.java
@@ -3,7 +3,11 @@ package sannakjellqvist.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sannakjellqvist.sfgpetclinic.model.Vet;
 import sannakjellqvist.sfgpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +24,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
